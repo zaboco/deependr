12bc6c8373935f1232601fa9f759898bd4a3965e
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2015-10-26 23:54:34 +0200

    add Container#store

--- src/Container.js
+++ src/Container.js
@@ -1,5 +1,12 @@
 'use strict'
 
 module.exports = class Container {
+  store(field, value) {
+    this[field] = value
+    return this
+  }
 
+  get(field) {
+    return this[field]
+  }
 }
--- test/Container.test.js
+++ test/Container.test.js
@@ -6,4 +6,34 @@ suite('Container', () => {
   test('exists', () => {
     should.exist(Container)
   })
+
+  const someObject = {}
+  let container
+  suite('#store', () => {
+    setup(() => {
+      container = new Container()
+    })
+
+    test('can store values', () => {
+      container.store('value', someObject)
+      container.get('value').should.equal(someObject)
+    })
+
+    test('stored value is accessible as getter accessor', () => {
+      container.store('value', someObject)
+      container.value.should.equal(someObject)
+    })
+
+    test('a value can be overridden', () => {
+      const otherObject = {}
+      container.store('value', someObject)
+      container.store('value', otherObject)
+      container.value.should.equal(otherObject)
+    })
+
+    test('returns the container itself', () => {
+      let storeResult = container.store('value', someObject)
+      storeResult.should.equal(container)
+    })
+  })
 })
