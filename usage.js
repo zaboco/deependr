0af3f9e54c6d5fd2446adf845903e2556c6f6112
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2015-10-28 08:58:33 +0200

    docs: add some basic usage example

--- usage.js
+++ usage.js
@@ -1,16 +1,6 @@
-# Deependr
-Dependency injection with nested contexts.
+'use strict'
 
-### Installation
-> assumes node `> v4.0.0`
-
-```sh
-npm i --save deependr
-```
-
-### Basic Usage
-```js
-const deependr = require('deependr')
+const deependr = require('.')
 const components = deependr.components
 
 let globals = deependr.container({
@@ -35,16 +25,3 @@ console.log(container.nestedContainer.nestedValue) // 'some nested value'
 
 container.unwrapPath('computedValue').set('innerValue', 'other inner value')
 console.log(container.computedValue) // 'other inner value'
-
-```
-
-> See also [`test/usecase.test`](test/usecase.test.js) for a more complex example
-
-### TODO
-- [ ] simplify API
-    + [ ] remove `unwrapPath` and make `unwrap` generic
-    + [ ] make `set` and `get` accept nested paths
-    + [ ] maybe remove `store`, `define`, `link` if `set` is enough
-- [ ] write API docs 
-- [ ] make contexts lazy
-- [ ] add CI
